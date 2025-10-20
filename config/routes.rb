96ff9b7cfb2dadfe12cd2e138b9d69479551b2f2
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-10-20 08:37:48 +0000

    [Fix] db-migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+
+  devise_for :admin_users, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
 
