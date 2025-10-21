1e067f98fd1983f47a073ed6bffcbd8c2cbe4d68
Ubuntu <ubuntu@ip-172-31-29-110.ap-northeast-1.compute.internal>
2025-10-21 12:30:24 +0000

    [Finish] feature-6.2.17

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -96,6 +96,8 @@ class Public::OrdersController < ApplicationController
     end
     
     def show
+      @order = Order.find(params[:id])
+      @order_details= OrderDetail.where(order_id: @order.id)
     end 
     
     def thanks
