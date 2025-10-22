5ab5a91b4d161546279f2f4bca1aa10083d984f2
Ubuntu <ubuntu@ip-172-31-29-110.ap-northeast-1.compute.internal>
2025-10-22 04:59:30 +0000

    [Finish] feature-6.2.13

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,15 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+  end
+
+  def update 
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    if @order.update(order_params)
+       @order_details.update_all(making_status: "製作待ち") if @order.status == "入金確認"
+    end
+      redirect_to admin_order_path(@order)
+  end
 end
