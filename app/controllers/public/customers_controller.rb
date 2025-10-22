1aaf104e9741a444b3e6581b1904f4764debd849
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-10-22 08:11:08 +0000

    [add] customer infomation edit page, addresses && add, edit address page

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,4 +12,25 @@ class Public::CustomersController < ApplicationController
     reset_session
     redirect_to new_customer_registration_path, notice: "退会手続きが完了しました。"
   end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path, notice: "会員情報を更新しました。"
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_customer
+    @customer = current_customer
+  end
+
+  def customer_params
+    params.require(:customer).permit(
+      :last_name, :first_name, :last_name_kana, :first_name_kana,
+      :postal_code, :address, :telephone_number, :email
+    )
+  end
 end
