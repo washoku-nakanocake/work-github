1c041cd2e37ce490cea0ad2a1a4040ee3defa609
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-10-23 23:21:50 +0000

    [Fix] 管理者/商品詳細画面修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
-  
-  enum status: { on_sale: 0, off_sale: 1 } #販売ステータス用enum
 
   with_options presence: true do
     validates :name
