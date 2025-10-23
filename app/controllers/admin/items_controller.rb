32136bd787115550f4773b2fe138ae098f8e566a
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-10-23 07:26:47 +0000

    [Add] 商品新規登録画面実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,29 @@ class Admin::ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to show
+    else
+      render new
+    end
+  end
+
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :price_without_tax, :is_active, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  
   enum status: { on_sale: 0, off_sale: 1 } #販売ステータス用enum
+
+  with_options presence: true do
+    validates :name
+    validates :detail
+    validates :price_without_tax
+    validates :is_active
+  end
 end
