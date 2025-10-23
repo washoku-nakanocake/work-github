c080a10344e0ae74f88dc15360248e88c70beac4
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-10-23 20:23:26 +0000

    [Add] Itemモデル&テーブル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :genre
   has_one_attached :image
   
   enum status: { on_sale: 0, off_sale: 1 } #販売ステータス用enum
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_23_071249) do
+ActiveRecord::Schema.define(version: 2025_10_23_200239) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(version: 2025_10_23_071249) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_genres_on_name"
+  end
+
   create_table "items", force: :cascade do |t|
     t.integer "genre_id", null: false
     t.string "name", null: false
@@ -92,6 +99,7 @@ ActiveRecord::Schema.define(version: 2025_10_23_071249) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["genre_id"], name: "index_items_on_genre_id"
+    t.index ["is_active"], name: "index_items_on_is_active"
     t.index ["name"], name: "index_items_on_name"
     t.index ["price_without_tax"], name: "index_items_on_price_without_tax"
   end
