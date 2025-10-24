bc3e582b910d5879aedf0062da33b5259a0a9d70
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-10-24 05:20:56 +0000

    [Add] 会員製品詳細画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,4 +4,8 @@ class Public::ItemsController < ApplicationController
     @items = Item.order(created_at: :desc).page(params[:page]).per(8) 
     @total_count = Item.count
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
